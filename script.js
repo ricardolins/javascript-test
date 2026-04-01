950b03dc4a45d8025772d8e92273a03ade459835
Ricardo Lins <ricardobarbosalins@hotmail.com>
2026-04-01 10:15:50 -0300

    new javascriot files

---
+++ script.js
@@ -0,0 +1,53 @@
+
+//////////// how to comment javascrit
+alert()
+
+let firstName = 'Ricardo';
+
+let last_name = 'Lins'
+
+
+
+// alert('hi recardo welcome to javascript');
+
+// console.log('hello world');
+
+// let recardo = 'amazingd';
+
+// console.log(recardo);
+
+// let age = 20;
+
+//////1. we cant use number before variable name
+// let 3age = 20;
+
+//////2. we never use & sign in variable name
+// let waseem&ricardo = 'web developer';
+
+//////3. we never use some reserved words
+// let new = 27;
+
+// /////////we can use this type of syntax for writing a variable name
+// let firstName = 'malik';
+
+// let last_name = 'abdullah';
+
+// let $hello = 29;
+
+// ///////////we have to choose meaningfull variable names
+// let job1 = 'programmer';
+// let job2 = 'teacher';
+
+// let firstJob = 'programmer';
+// let currentJob = 'teacher';
+
+///////////////////////////////Data type//////////////////
+
+/*
+
+/////// In every programming language there are different types of values
+////// In js there are total 5 types of DATA TYPES
+
+//////1. Number: Floating point numbers (used for decimals and integers)
+let age1 = 23;
+
